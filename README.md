ac64a40924771a415e01d52db4c1d557ecd3a99f
Andrew Connell <me@andrewconnell.com>
2020-05-27 06:59:00 -0400

    update name of input param
    fixes #1

--- README.md
+++ README.md
@@ -9,16 +9,16 @@ This is a GitHub Action that submits a reindex request to an Azure Cognitive Sea
 This action requires the following inputs:
 
 - **azure-search-instance**: Name of the Azure Cognitive Search resource
-- **azure-search-index**: Name of the Azure Cognitive Search index
+- **azure-search-indexer**: Name of the Azure Cognitive Search indexer
 - **azure-search-admin-key**: Name of the Azure Cognitive Search index *(store this as a [secret](https://help.github.com/en/actions/configuring-and-managing-workflows/creating-and-storing-encrypted-secrets)*)
 
 ## Example Usage
 
 ```yml
 - name: Reindex Azure Cognitive Search index
-  uses: andrewconnell/azure-search-index@1.0.0
+  uses: andrewconnell/azure-search-index@1.0.1
   with:
     azure-search-instance: my-search-resource-name
-    azure-search-index: my-search-index-name
+    azure-search-indexer: my-search-indexer-name
     azure-search-admin-key: ${{ secrets.SEARCH_ADMIN_KEY }}
 ```
--- src/index.ts
+++ src/index.ts
@@ -8,11 +8,11 @@ async function run(): Promise<void> {
   try {
     core.debug('Reading settings...');
     const SEARCH_INSTANCE = core.getInput('azure-search-instance');
-    const SEARCH_INDEX = core.getInput('azure-search-index');
+    const SEARCH_INDEXER = core.getInput('azure-search-indexer');
     const SEARCH_ADMIN_KEY = core.getInput('azure-search-admin-key');
 
     // create URL for search index call
-    const ENDPOINT = `https://${SEARCH_INSTANCE}.search.windows.net/indexers/${SEARCH_INDEX}/run?api-version=${API_VERSION}`;
+    const ENDPOINT = `https://${SEARCH_INSTANCE}.search.windows.net/indexers/${SEARCH_INDEXER}/run?api-version=${API_VERSION}`;
 
     // setup headers
     const HEADERS = {
@@ -25,9 +25,9 @@ async function run(): Promise<void> {
       core.debug(`Sending HTTP POST to ${ENDPOINT}`)
 
       await Axios.post(ENDPOINT, undefined, { headers: HEADERS } );
-      core.info(`Search index '${SEARCH_INDEX}' reindexed.`);
+      core.info(`Search index '${SEARCH_INDEXER}' reindexed.`);
     } catch (error) {
-      core.setFailed(`Failed to reindex the search index ${SEARCH_INDEX}: ${error.message}`);
+      core.setFailed(`Failed to reindex the search index ${SEARCH_INDEXER}: ${error.message}`);
     }
   } catch (error) {
     core.setFailed('Unknown error occurred: ${error.message}');
